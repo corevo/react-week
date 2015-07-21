ee22ed542c3ec5b81e81b1a7c0be6e169d60e867
Tomer <gmtomer@gmail.com>
2015-07-21 15:52:36 +0300

    o.0 todo

--- day1todo/app/app.js
+++ day1todo/app/app.js
@@ -1,1 +1,51 @@
 var React = require('react');
+
+var TodoInput = React.createClass({
+    render () {
+        return (
+            <input type="text" placeholder="do something?!" onKeyPress={this.props.event} />
+        );
+    }
+});
+
+var TodoItem = React.createClass({
+    render () {
+        return (
+            <li value={this.props.item}>{this.props.item} <a onClick={this.props.event}>Remove</a></li>
+        );
+    }
+});
+
+var TodoList = React.createClass({
+    getInitialState () {
+        return {
+            items: [],
+            input: <TodoInput event={this.addItem} />
+        };
+    },
+    addItem (event) {
+        if (event.key === 'Enter') {
+            this.state.items.push(event.target.value);
+            event.target.value = '';
+            this.forceUpdate();
+        }
+    },
+    removeItem (event) {
+        this.state.items.pop(event.target.value);
+        this.forceUpdate();
+    },
+    render () {
+        return (
+            <div className="container">
+                {this.state.input}
+                <ul className="list">
+                    {this.state.items.map((item) => (
+                        <TodoItem item={item} event={this.removeItem} />
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+});
+
+React.render(<TodoList />, document.getElementById('app'));
